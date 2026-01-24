3ced38a5c456c7b18b344e6a0bc7173a98f2d78b
amame04 <ameame2t@gmail.com>
2026-01-24 19:21:43 +0900

    add summary

---
+++ photos.js
@@ -0,0 +1,154 @@
+const photos = {
+  "Horizon Perfekt": [
+    {
+      "filename": "000031.JPG",
+      "summary": "2025/11 - FUJIFILM 400 - Enoshima Kanagawa"
+    },
+    {
+      "filename": "000033.JPG",
+      "summary": "2025/11 - FUJIFILM 400 - Enoshima Kanagawa"
+    },
+    {
+      "filename": "000027.JPG",
+      "summary": "2025/11 - FUJIFILM 400 - Akihabara Tokyo"
+    },
+    {
+      "filename": "000048.JPG",
+      "summary": "2025/11 - FUJIFILM 400 - Sengawa Tokyo"
+    },
+    {
+      "filename": "200003.JPG",
+      "summary": "2025/12 - FUJIFILM 400 - Kichijoji Tokyo"
+    },
+    {
+      "filename": "200011.JPG",
+      "summary": "2025/12 - FUJIFILM 400 - Kichijoji Tokyo"
+    },
+    {
+      "filename": "200027.JPG",
+      "summary": "2025/12 - FUJIFILM 400 - Chofu Tokyo"
+    },
+    {
+      "filename": "200033.JPG",
+      "summary": "2025/12 - FUJIFILM 400 - Chofu Tokyo"
+    },
+    {
+      "filename": "200043.JPG",
+      "summary": "2025/12 - FUJIFILM 400 - Chofu Tokyo"
+    },
+    {
+      "filename": "300007.JPG",
+      "summary": "2026/01 - FUJIFILM 400 - Akiruno Tokyo"
+    },
+    {
+      "filename": "300013.JPG",
+      "summary": "2026/01 - FUJIFILM 400 - Akiruno Tokyo"
+    },
+    {
+      "filename": "300023.JPG",
+      "summary": "2026/01 - FUJIFILM 400 - Akiruno Tokyo"
+    }
+  ],
+  "GR IV": [
+    {
+      "filename": "R0000165.JPG",
+      "summary": "2025/12 - Kyoto"
+    },
+    {
+      "filename": "R0000168.JPG",
+      "summary": "2025/12 - Kyoto"
+    },
+    {
+      "filename": "R0000264.JPG",
+      "summary": "2025/12 - Tottori"
+    },
+    {
+      "filename": "R0000273.JPG",
+      "summary": "2025/12 - Tottori"
+    },
+    {
+      "filename": "R0000293.JPG",
+      "summary": "2025/12 - Tottori"
+    },
+    {
+      "filename": "R0000451.JPG",
+      "summary": "2025/12 - Nagato"
+    },
+    {
+      "filename": "R0000459.JPG",
+      "summary": "2025/12 - Nagato"
+    },
+    {
+      "filename": "R0000463.JPG",
+      "summary": "2025/12 - Nagato"
+    },
+    {
+      "filename": "R0000498.JPG",
+      "summary": "2025/12 - Shimonoseki"
+    }
+  ],
+  "Konica C35": [
+    {
+      "filename": "000009.JPG",
+      "summary": "2025/12 - Kodak Gold 200 - Kichijoji Tokyo"
+    },
+    {
+      "filename": "000017.JPG",
+      "summary": "2025/12 - Kodak Gold 200 - Kichijoji Tokyo"
+    },
+    {
+      "filename": "000018.JPG",
+      "summary": "2025/12 - Kodak Gold 200 - Kichijoji Tokyo"
+    },
+    {
+      "filename": "000020.JPG",
+      "summary": "2025/12 - Kodak Gold 200 - Amanohashidate Kyoto"
+    },
+    {
+      "filename": "000024.JPG",
+      "summary": "2025/12 - Kodak Gold 200 - Tottori"
+    },
+    {
+      "filename": "000025.JPG",
+      "summary": "2025/12 - Kodak Gold 200 - Tottori"
+    },
+    {
+      "filename": "000028.JPG",
+      "summary": "2025/12 - Kodak Gold 200 - Tottori"
+    }
+  ],
+  "GX7 mk2": [
+    {
+      "filename": "P1010572.jpg",
+      "summary": "2020/10 - Okutama Tokyo"
+    },
+    {
+      "filename": "P1010707.jpg",
+      "summary": "2020/10 - Okutama Tokyo"
+    },
+    {
+      "filename": "P1010741.jpg",
+      "summary": "2020/10 - Okutama Tokyo"
+    },
+    {
+      "filename": "P1010847-2.jpg",
+      "summary": "2020/11 - Akiruno Tokyo"
+    },
+    {
+      "filename": "P1020007.jpg",
+      "summary": "2020/11 - Akasaka Tokyo"
+    },
+    {
+      "filename": "P1020026.jpg",
+      "summary": "2020/11 - Akasaka Tokyo"
+    },
+    {
+      "filename": "P1020044.jpg",
+      "summary": "2020/11 - Akiruno Tokyo"
+    },
+    {
+      "filename": "P1020133.jpg",
+      "summary": "2021/02 - Akiruno Tokyo"
+    }
+  ]
+}
