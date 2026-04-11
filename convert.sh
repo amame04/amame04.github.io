9a6317437ae2401375cc71bb985b06531a782e47
amame04 <ameame2t@gmail.com>
2026-04-11 23:45:12 +0900

    webpに変換して表示

---
+++ convert.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SRC="images"
+DST_THUMB="thumbs"
+
+find "$SRC" -type f \( -iname "*.jpg" -o -iname "*.jpeg" \) | while read -r f; do
+  rel="${f#$SRC/}"
+  out="${rel}.webp"
+
+  thumb_out="$DST_THUMB/$out"
+
+  mkdir -p "$(dirname "$thumb_out")"
+
+  convert "$f" -auto-orient -resize 2000x2000\> -quality 80 "$thumb_out"
+done
